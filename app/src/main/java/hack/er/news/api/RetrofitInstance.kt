b4510e222f2ac582963b48e75a70290b16001082
Yash-Garg <yash.garg@outlook.in>
2021-08-10 23:58:24 +0530

    fix: use moshi-metadata-reflect

--- app/src/main/java/hack/er/news/api/RetrofitInstance.kt
+++ app/src/main/java/hack/er/news/api/RetrofitInstance.kt
@@ -1,13 +1,13 @@
 package hack.er.news.api
 
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dev.zacsweers.moshix.reflect.MetadataKotlinJsonAdapterFactory
 import hack.er.news.utils.Constants
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitInstance {
-    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    private val moshi = Moshi.Builder().add(MetadataKotlinJsonAdapterFactory()).build()
 
     private val retrofit = Retrofit.Builder().baseUrl(Constants.BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create(moshi)).build()
