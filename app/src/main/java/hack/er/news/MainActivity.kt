34e3887f7f1225b8f22ac05d097304ce10f8b843
Yash-Garg <yash.garg@outlook.in>
2021-08-12 14:08:31 +0530

    chore(states): add loadStates in separate lifecycleScope

--- app/src/main/java/hack/er/news/MainActivity.kt
+++ app/src/main/java/hack/er/news/MainActivity.kt
@@ -1,10 +1,11 @@
 package hack.er.news
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import dagger.hilt.android.AndroidEntryPoint
 import hack.er.news.adapter.ArticleAdapter
 import hack.er.news.databinding.ActivityMainBinding
@@ -25,11 +26,25 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val recyclerView = binding.recyclerView
+        val loadingIndicator = binding.loadingIndicator
+        val errorView = binding.errorView.root
+
         lifecycleScope.launch {
             mainViewModel.getArticles().collectLatest { pagingData ->
-                binding.loadingIndicator.visibility = View.GONE
-                binding.recyclerView.adapter = articleAdapter
+                recyclerView.adapter = articleAdapter
                 articleAdapter.submitData(pagingData)
+            }
+        }
+
+        /** Separate launch scope for states
+        because it was getting blocked due
+        to [submitData] launch scope **/
+        lifecycleScope.launch {
+            articleAdapter.loadStateFlow.collectLatest { loadStates ->
+                loadingIndicator.isVisible = loadStates.refresh is LoadState.Loading
+                errorView.isVisible = loadStates.refresh is LoadState.Error
+                recyclerView.isVisible = loadStates.refresh is LoadState.NotLoading
             }
         }
     }
